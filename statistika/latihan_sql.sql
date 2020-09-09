15c05f7de05c2a3e0e70480d0b0a0b148c1548e0
LENOVO <julius.risky@gmail.com>
2020-09-09 16:31:09 +0700

    latihan join dari skill academy

---
+++ statistika/latihan_sql.sql
@@ -0,0 +1,17 @@
+SELECT 
+t.*,
+ap.name AS Produk_name
+FROM transaction t
+JOIN(
+SELECT 
+	produk_id,
+	'produk' AS produk_type,
+	NAME
+FROM produk
+UNION all
+SELECT
+	produk_id,
+	'virtual_produk' AS produk_type,
+	NAME
+FROM virtual_produk
+) ap ON ap.produk_id = t.produk_id AND ap.produk_type = t.produk_type;
