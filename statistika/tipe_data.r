b67899e3ef59b9482f86801e8c1d154bbfaf355a
LENOVO <julius.risky@gmail.com>
2020-09-10 16:18:33 +0700

    latihan tipe data r

--- statistika/tipe_data.r
+++ statistika/tipe_data.r
@@ -7,3 +7,14 @@ m = matrix(1:12,nrow=3)
 #array
 a = array(1:24,c(3,4,2))
 
+#data frame
+
+user <-data.frame(
+  names = name,
+  gender = c('male','male','male','female'),
+  age = c(10,22,32,22)
+)
+
+#tipe data list
+
+l = list(a,name,user)
