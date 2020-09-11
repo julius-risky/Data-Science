fe3950d43a613f74c052a2ba634329531761946e
LENOVO <julius.risky@gmail.com>
2020-09-11 20:45:43 +0700

    latihan dasar visualisasi di R

--- statistika/visualisasi_data_r.r
+++ statistika/visualisasi_data_r.r
@@ -14,3 +14,20 @@ ggplot(dataset,aes(sales))+geom_histogram(bins =10)
 
 ggplot(dataset,aes(x=segment,y=sales)) + geom_bar(stat = 'identity', width=0.5,aes(fill=category))
 
+#pie chart
+
+sales_per_segment<- dataset %>%group_by(segment) %>% summarise(total_sales=sum(sales))
+ggplot(sale#pie chart
+
+sales_per_segment<- dataset %>%group_by(segment) %>% summarise(total_sales=sum(sales))
+ggplot(sales_per_segment,aes(x='',y=total_sales,fill=segment)) + geom_bar(stat='identity',width = 5)+coord_polar('y',start=0)
+
+#line chart
+dataset$order_date <-as.Date(dataset$order_date)
+dataset$order_month <- as.Date(cut(dataset$order_date, breaks = 'month'))
+
+ggplot(dataset, aes(x= order_month,y=sales))+stat_summary(fun.y = sum,geom='line')
+
+mount_sales <- dataset%>% group_by(order_month)%>%summarise(sales=sum(sales))
+
+ggplot(mount_sales,aes(x=order_month,y=sales)) + geom_line()+geom_point(colour='blue')
