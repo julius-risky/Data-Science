5f378b7b6064a9246a56e69365ad88e6c255b916
LENOVO <julius.risky@gmail.com>
2020-09-11 21:01:28 +0700

    menambahkan select()

--- statistika/operation_dataframe.r
+++ statistika/operation_dataframe.r
@@ -2,6 +2,8 @@ dataset <- read.csv('Assessment_Statistics_foundation_ Dataset_superstore_simple
 
 nrow(dataset)
 ncol(dataset)
+#memilih data
+dr <- select(dataset,-c(order_id)) #-c() untuk memilih kecuali kolom tersebut
 #memfilter data
 dr<- filter(dataset, segment=='Consumer' & profit >0)
 
