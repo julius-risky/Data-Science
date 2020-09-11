8c2da289cab53534464b49a9773af071204e2f54
LENOVO <julius.risky@gmail.com>
2020-09-11 20:47:56 +0700

    latihan data summarization

--- statistika/data_summarization.r
+++ statistika/data_summarization.r
@@ -14,3 +14,19 @@ data_b <- dataset %>% filter(category=='Technology') %>%
 intersect(data_a,data_b)
 
 setdiff(data_a,data_b)
+
+#penggabungan berdasarkan kolom
+
+data_c <- select(data_a,c(order_id,segment,sub_category,sales))%>% head(9)
+
+data_d <- select(data_b,c(order_id,segment,sub_category,category,order_date))
+
+bind_cols(data_c,data_b)
+
+#join/ menggabungkan yg sama
+
+inner_join(data_c,data_d)
+
+full_join(data_c,data_d)
+
+left_join(data_c,data_d)
