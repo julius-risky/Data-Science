ae78f30231e50fba1c121a9e73d2b5388b3fb61e
LENOVO <julius.risky@gmail.com>
2020-09-11 20:31:09 +0700

    latihan dasar visualisasi di R

--- statistika/visualisasi_data_r.r
+++ statistika/visualisasi_data_r.r
@@ -6,3 +6,11 @@ dataset <- read.csv('Assessment_Statistics_foundation_ Dataset_superstore_simple
 
 ggplot(dataset,aes(x=sales,y =profit))+geom_point(colour='blue')
 
+#visualisasi histogram
+
+ggplot(dataset,aes(sales))+geom_histogram(bins =10)
+
+#bar chart
+
+ggplot(dataset,aes(x=segment,y=sales)) + geom_bar(stat = 'identity', width=0.5,aes(fill=category))
+
