c28c7ec30240994d68cb23e98ba207e7ff619bf6
LENOVO <julius.risky@gmail.com>
2020-09-28 22:57:49 +0700

    exploratory data analys by dqlab

---
+++ statistika/exploratory_DA.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#membaca data csv
+df = pd.read_csv('F:\Latihan-Data-Science\statistika\Assessment_Statistics_foundation_ Dataset_superstore_simple.csv')
+
+#menampilkan ukuran data
+print(df.shape())
+
