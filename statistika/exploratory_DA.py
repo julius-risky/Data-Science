39b799eec11d8f948ab32352ae5f16196f07b294
LENOVO <julius.risky@gmail.com>
2020-09-29 20:37:43 +0700

    latihan eploratory data frame by dqlab

--- statistika/exploratory_DA.py
+++ statistika/exploratory_DA.py
@@ -36,7 +36,7 @@ order_df = pd.read_csv("order.csv")
 # Hitung quartile 1
 Q1 = order_df[['product_weight_gram']].quantile(0.25)
 # Hitung quartile 3
-Q3 = rder_df[['product_weight_gram']].quantile(0.75)
+Q3 = order_df[['product_weight_gram']].quantile(0.75)
 # Hitung inter quartile range dan cetak ke console
 IQR = Q3-Q1
 print(IQR)
