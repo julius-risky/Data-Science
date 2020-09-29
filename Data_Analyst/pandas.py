b32809596c05cb44bb2e587c88dd745c97c0bb4d
LENOVO <julius.risky@gmail.com>
2020-09-29 20:43:03 +0700

    latihan data frame dan series by dqlab

--- Data_Analyst/pandas.py
+++ Data_Analyst/pandas.py
@@ -44,3 +44,14 @@ print(number_list.unique())
 print("[8] attribute .index")
 print("    Index number_list:", number_list.index)
 print("    Index matrix_list:", matrix_list.index)	
+# [9] attribute .columns
+print("[9] attribute .columns")
+print("    Column matrix_list:", matrix_list.columns)
+# [10] attribute .loc
+print("[10] attribute .loc")
+print("    .loc[0:1] pada number_list:", number_list.loc[0:1])
+print("    .loc[0:1] pada matrix_list:", matrix_list.loc[0:1])
+# [11] attribute .iloc
+print("[11] attribute .iloc")
+print("    iloc[0:1] pada number_list:", number_list.iloc[0:1])
+print("    iloc[0:1] pada matrix_list:", matrix_list.iloc[0:2,2])	
