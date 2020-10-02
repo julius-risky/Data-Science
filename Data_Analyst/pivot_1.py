de0bc10e791f3e4eb77a4ca86746f2d323290db9
LENOVO <julius.risky@gmail.com>
2020-10-02 21:28:54 +0700

    latihan pivot table dqlab

---
+++ Data_Analyst/pivot_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+
+# Pivoting with single column measurement
+pivot1 = data.pivot(index='murid',columns='pelajaran',values='nilai')
+print('Pivoting with single column measurement:\n', pivot1)
